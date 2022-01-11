1dd8f13f0e45715926b7c9cb0169b817e74268a9
MarinaAparicioSantos <marina.aparicio.santos@iespoligonosur.org>
2022-01-12 00:23:40 +0100

    8

--- ejercicio8/includes/class.local.php
+++ ejercicio8/includes/class.local.php
@@ -17,29 +17,55 @@ class Local
         $this->calle = $calle;
         $this->numeroDePlantas = $numeroDePlantas;
         $this->area = $area;
-        var_dump($dimensiones instanceof Dimensiones);
         $this->dimensiones = $dimensiones;
     }
 
+    //getter y setter
     function getDimensiones()
     {
         return $this->dimensiones;
-    }
-
-    function setDimensiones($dimensiones)
-    {
-        $this->dimensiones = $dimensiones;
     }
 
     function getArea()
     {
         return $this->area;
     }
+    
 
-    function setArea($area)
+    public function __set($atributo, $valor)
     {
-        $this->area = $area;
+
+        //el constructor llama directamente a los getters y los setters.
+
+        switch ($atributo) {
+            case "dimensiones":
+                if ($this->dimensiones instanceof Dimensiones) {
+
+                    $this->$atributo = $valor;
+                } else {
+                    echo "Dimensiones no es instancia de dimensiones.";
+                }
+                break;
+
+            case "numeroDePlantas":
+                if ($valor > 1 && $valor < 10) {
+
+                    $this->$atributo = $valor;
+                } else {
+                    echo "El valor no puede ser menor que 1 ni mayor que 10.";
+                }
+                break;
+        };
+    }
+
+    function __toString()
+    {
+        echo "<p>Ciudad: $this->ciudad <br></p><p>Calle:$this->calle<br></p><p>Plantas: 
+            $this->numeroDePlantas<br></p><p>Dimensiones: $this->dimensiones<br></p>";
+    }
+
+    public function __clone()
+    {
+        $dimensionesCopy = clone $this->dimensiones;
     }
 }
-
-var_dump($dimensiones instanceof Dimensiones);
